e70da69b28a02a224a0ba9181e9f3261c929a2c4
Pooja <poojapurohit939@gmail.com>
2024-07-26 08:43:14 +0530

    second commit

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,9 +2,15 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../../images/logo.png';
 import logo2 from '../../images/logo2.png'
+import  { useState } from 'react';
 import './Navbar.css';
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
   return (
     <nav className="navbar">
       <Link to="/" className="nav_logo">
@@ -12,14 +18,19 @@ const Navbar = () => {
         <img src={logo} alt="Logo" className='logo1' />
         {/* <img src={logo} alt="Logo" className='logo2' /> */}
       </Link>
-      <ul className="nav_menu">
+      <div className={`nav_menu ${menuOpen ? 'active' : ''}`}>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/products">Products</Link></li>
         <li><Link to="/cart">Cart</Link></li>
         <li><Link to="/about">About Us</Link></li>
         <li><Link to="/login">Login</Link></li>
         <li><Link to="/register">SignUp</Link></li>
-      </ul>
+      </div>
+      <div className="hamburger" onClick={toggleMenu}>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
     </nav>
   );
 };
